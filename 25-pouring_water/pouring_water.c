928b26b89538b5d6ec4bc877641e800156ed7bb0
breno.araujo <breno.araujo@geosales.com.br>
2022-08-08 21:15:36 -0300

    fila implementada - insercao e remocao

--- 25-pouring_water/pouring_water.c
+++ 25-pouring_water/pouring_water.c
@@ -25,34 +25,84 @@ void *initQueue() {
     queue_size = 0;
 }
 
-void insertQueue(state *s) {
+void insert(state *s) {
     if (queue_size == 0) {
         s->next = NULL;
         head = s;
-        printf("%p\n", head->next);
-        printf("-------------------\n");
     } else if (queue_size == 1) {
         tail = s;
-        printf("head: %d\n", head->x);
-        printf("tail: %d\n", tail->x);
-
+        head->next = tail;
     } else {
-
+        s->next = NULL;
+        tail->next = s;
+        tail = s;
     }
     queue_size++;
+}
+
+state *dequeue() {
+    state *removed;
+    int count = 0;
+    if (queue_size == 0) {
+        return NULL;
+    } else if (queue_size == 1) {
+        removed = head;
+        head = NULL;
+        tail = NULL;
+        queue_size--;
+        return removed;
+    } else {
+        removed = tail;
+        tail = head;
+    }
+    queue_size--;
+    while(count < queue_size-1) {
+        tail = tail->next;
+        count++;
+    }
+    tail->next = NULL;
+    return removed;
+}
+
+void printQueue() {
+    state *runner = head;
+    while(runner != NULL) {
+        printf("%d\n", runner->x);
+        runner = runner->next;
+    }
 }
 
 int main() {
     initQueue();
     state inicio = {0,0,0};
     state s2 = {1,1,0};
-    // state s3 = {2,2,0};
-    // state s4 = {3,3,0};
-    // state s5 = {4,4,0};
-    insertQueue(&inicio);
-    insertQueue(&s2);
-    // insertQueue(head, s3);
-    // insertQueue(head, s4);
-    // insertQueue(head, s5);
+    state s3 = {2,2,0};
+    state s4 = {3,3,0};
+    state s5 = {4,4,0};
+    insert(&inicio);
+    insert(&s2);
+    insert(&s3);
+    insert(&s4);
+    insert(&s5);
+
+    printQueue();
+
+    state *st0 = dequeue();
+    printQueue();
+    printf("removido: %d", st0->x);
+    printf("--------------------------\n");
+
+    st0 = dequeue();
+    printQueue();    
+    printf("removido: %d", st0->x);
+    printf("--------------------------\n");
+
+    st0 = dequeue();
+    printQueue();
+    printf("removido: %d", st0->x);
+    printf("--------------------------\n");
+
+    // printf("%d", st2->x);
+
     return 0;
 }
